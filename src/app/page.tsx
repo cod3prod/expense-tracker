53327d782bcaf8929a3a0018413881c146994904
cod3prod <cod3prod@gmail.com>
2024-12-06 17:12:39 +0900

    💄 Fix main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ExpenseTracker from "@/components/expense-tracker";
 
 export default function Page() {
   return (
-    <main className="grow py-4 max-w-4xl mx-auto">
+    <main className="grow w-full max-w-4xl mx-auto p-4">
       <ExpenseTracker />
     </main>
   )
